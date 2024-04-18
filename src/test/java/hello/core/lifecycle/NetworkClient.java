6562ed824cee5acf2899ade7d64f1d127f00f789
geunhokinn <gghh1220@gmail.com>
2024-04-18 17:11:49 +0900

    test: 빈 생명주기 콜백 테스트

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,33 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    private String url; // 서버 url
+
+    // 접속 url 출력
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // url은 외부에서 주입
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    // 서비스 연결이 된 상태에서 호출할 수 있음
+    public void call(String message) {
+        System.out.println("call : " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close : " + url);
+    }
+}
